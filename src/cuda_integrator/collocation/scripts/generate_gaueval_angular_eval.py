247c9c9abbc66d07f927fd2f68b808fbfd47e341
David Williams-Young <dbwy@lbl.gov>
2020-08-11 15:53:08 -0700

    Reduced default LMAX in CUDA collocation to 3

--- src/cuda_integrator/collocation/scripts/generate_gaueval_angular_eval.py
+++ src/cuda_integrator/collocation/scripts/generate_gaueval_angular_eval.py
@@ -4,8 +4,8 @@ from gaueval_angular import generate_spherical_angular, generate_cartesian_angul
 
 from io import StringIO
 
-
 L_max = 6
+if len(sys.argv) > 1: L_max = int(sys.argv[1])
 
 #sphr_bf_body = []
 #sphr_bf_d1_body = []
