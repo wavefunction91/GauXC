9400fc0c055846221f2581a761a0c50e04a02a8f
Abhishek Bagusetty <abagusetty@anl.gov>
2020-09-08 19:58:45 +0000

    port zmat functions

---
+++ src/integrator/sycl/sycl_zmat.cpp
@@ -0,0 +1,148 @@
+#include "sycl_zmat.hpp"
+#include <gauxc/util/div_ceil.hpp>
+
+namespace GauXC      {
+namespace integrator {
+namespace sycl       {
+
+template <typename T>
+void zmat_lda_kernel( size_t           ntasks,
+                      XCTaskDevice<T>* tasks_device ,
+                      cl::sycl::nd_item<3> item_ct) {
+
+  const int batch_idx = item_ct.get_group(0);
+  if( batch_idx >= ntasks ) return;
+
+  auto& task = tasks_device[ batch_idx ];
+  const auto npts            = task.npts;
+  const auto nbf             = task.nbe;
+  const auto* vrho_device    = task.vrho;
+
+  const auto* basis_eval_device = task.bf;
+
+  auto* z_matrix_device = task.zmat;
+
+  const int tid_x = item_ct.get_group(2) * item_ct.get_local_range().get(2) +
+                    item_ct.get_local_id(2);
+  const int tid_y = item_ct.get_group(1) * item_ct.get_local_range().get(1) +
+                    item_ct.get_local_id(1);
+
+  if( tid_x < nbf and tid_y < npts ) {
+    const size_t ibfoff = tid_x + tid_y * nbf;
+    const double fact = 0.5 * vrho_device[tid_y];
+
+    z_matrix_device[ tid_x + tid_y * nbf ] = fact * basis_eval_device[ ibfoff ];
+  }
+}
+
+template <typename T>
+void zmat_lda_sycl(size_t ntasks, int32_t max_nbf, int32_t max_npts,
+                   XCTaskDevice<T> *tasks_device, cl::sycl::queue *stream) {
+
+  cl::sycl::range<3> threads(32, 32, 1);
+  cl::sycl::range<3> blocks(util::div_ceil(max_nbf, threads.get(2)),
+                            util::div_ceil(max_npts, threads.get(1)),
+                            ntasks);
+
+  stream->submit([&](sycl::handler &cgh) {
+    auto global_range = blocks * threads;
+
+    cgh.parallel_for(cl::sycl::nd_range<3>(cl::sycl::range<3>(global_range.get(2),
+                                                              global_range.get(1),
+                                                              global_range.get(0)),
+                                           cl::sycl::range<3>(threads.get(2),
+                                                              threads.get(1),
+                                                              threads.get(0))),
+
+        [=](cl::sycl::nd_item<3> item_ct) {
+          zmat_lda_kernel(ntasks, tasks_device, item_ct);
+        });
+  });
+}
+
+template
+void zmat_lda_sycl( size_t                ntasks,
+                    int32_t               max_nbf,
+                    int32_t               max_npts,
+                    XCTaskDevice<double>* tasks_device,
+                    cl::sycl::queue*      stream );
+
+template <typename T>
+void zmat_gga_kernel( size_t           ntasks,
+                      XCTaskDevice<T>* tasks_device ,
+                      cl::sycl::nd_item<3> item_ct) {
+
+  const int batch_idx = item_ct.get_group(0);
+  if( batch_idx >= ntasks ) return;
+
+  auto& task = tasks_device[ batch_idx ];
+  const auto npts            = task.npts;
+  const auto nbf             = task.nbe;
+  const auto* vrho_device    = task.vrho;
+  const auto* vgamma_device  = task.vgamma;
+  const auto* den_x_eval_device = task.ddenx;
+  const auto* den_y_eval_device = task.ddeny;
+  const auto* den_z_eval_device = task.ddenz;
+
+  const auto* basis_eval_device = task.bf;
+  const auto* dbasis_x_eval_device = task.dbfx;
+  const auto* dbasis_y_eval_device = task.dbfy;
+  const auto* dbasis_z_eval_device = task.dbfz;
+
+  auto* z_matrix_device = task.zmat;
+
+  const int tid_x = item_ct.get_group(2) * item_ct.get_local_range().get(2) +
+                    item_ct.get_local_id(2);
+  const int tid_y = item_ct.get_group(1) * item_ct.get_local_range().get(1) +
+                    item_ct.get_local_id(1);
+
+  if( tid_x < nbf and tid_y < npts ) {
+
+    const size_t ibfoff = tid_x + tid_y * nbf;
+    const double fact_1 = 0.5 * vrho_device[tid_y]  ;
+    const double fact_2 = 2.0 * vgamma_device[tid_y];
+
+    const double dx = den_x_eval_device[ tid_y ] * dbasis_x_eval_device[ ibfoff ];
+    const double dy = den_y_eval_device[ tid_y ] * dbasis_y_eval_device[ ibfoff ];
+    const double dz = den_z_eval_device[ tid_y ] * dbasis_z_eval_device[ ibfoff ];
+
+    z_matrix_device[ tid_x + tid_y * nbf ] =
+      fact_1 * basis_eval_device[ ibfoff ] + fact_2 * ( dx + dy + dz );
+
+  }
+}
+
+template <typename T>
+void zmat_gga_sycl(size_t ntasks, int32_t max_nbf, int32_t max_npts,
+                   XCTaskDevice<T> *tasks_device, cl::sycl::queue *stream) {
+
+  cl::sycl::range<3> threads(32, 32, 1);
+  cl::sycl::range<3> blocks(util::div_ceil(max_nbf, threads.get(2)),
+                            util::div_ceil(max_npts, threads.get(1)),
+                            ntasks);
+
+  stream->submit([&](cl::sycl::handler &cgh) {
+          auto global_range = blocks * threads;
+
+          cgh.parallel_for(cl::sycl::nd_range<3>(cl::sycl::range<3>(global_range.get(2),
+                                                                    global_range.get(1),
+                                                                    global_range.get(0)),
+                                                 cl::sycl::range<3>(threads.get(2),
+                                                                    threads.get(1),
+                                                                    threads.get(0))),
+
+                           [=](cl::sycl::nd_item<3> item_ct) {
+                               zmat_gga_kernel(ntasks, tasks_device, item_ct);
+                           });
+      });
+}
+template
+void zmat_gga_sycl( size_t                ntasks,
+                    int32_t               max_nbf,
+                    int32_t               max_npts,
+                    XCTaskDevice<double>* tasks_device,
+                    cl::sycl::queue*      stream );
+
+}
+}
+}
---
+++ src/integrator/sycl/sycl_zmat.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <CL/sycl.hpp>
+#include <gauxc/xc_task.hpp>
+
+namespace GauXC      {
+namespace integrator {
+namespace sycl       {
+
+using namespace GauXC::sycl;
+
+template <typename T>
+void zmat_lda_sycl(size_t ntasks, int32_t max_nbf, int32_t max_npts,
+                   XCTaskDevice<T> *tasks_device, cl::sycl::queue *stream);
+
+template <typename T>
+void zmat_gga_sycl(size_t ntasks, int32_t max_nbf, int32_t max_npts,
+                   XCTaskDevice<T> *tasks_device, cl::sycl::queue *stream);
+}
+}
+}
