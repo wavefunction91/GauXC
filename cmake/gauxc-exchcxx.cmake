26ede13a5226d8bfe86b86ebe54b51e6b26be337
David Williams-Young <dbwy@lbl.gov>
2020-09-21 18:13:48 +0000

    Added SYCL propagation / check against ExchCXX installation + build

--- cmake/gauxc-exchcxx.cmake
+++ cmake/gauxc-exchcxx.cmake
@@ -2,6 +2,7 @@ find_package( ExchCXX QUIET )
 if( NOT ${ExchCXX_FOUND} )
 
   set( EXCHCXX_ENABLE_CUDA  ${GAUXC_ENABLE_CUDA} CACHE BOOL "" )
+  set( EXCHCXX_ENABLE_SYCL  ${GAUXC_ENABLE_SYCL} CACHE BOOL "" )
   set( EXCHCXX_ENABLE_TESTS OFF                  CACHE BOOL "" )
 
   FetchContent_Declare(
@@ -19,6 +20,10 @@ else()
 
   if( ${GAUXC_ENABLE_CUDA} AND NOT ${EXCHCXX_ENABLE_CUDA} )
     message( FATAL_ERROR "GauXC CUDA BINDINGS REQUIRE ExchCXX CUDA Bindings" )
+  endif()
+
+  if( ${GAUXC_ENABLE_SYCL} AND NOT ${EXCHCXX_ENABLE_SYCL} )
+    message( FATAL_ERROR "GauXC SYCL BINDINGS REQUIRE ExchCXX SYCL Bindings" )
   endif()
 
 endif()
