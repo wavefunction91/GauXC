ad5fa6a0eac83a6bbfc698abd2c30c44f5cb0a5e
David Clark <daclark@nvidia.com>
2020-10-13 14:25:57 -0700

    Adds pack/inc optimization

--- src/integrator/integrator_common.cxx
+++ src/integrator/integrator_common.cxx
@@ -40,8 +40,49 @@ std::vector< std::pair<int32_t, int32_t> >
     submat_map.back().second = 
       basis.shell_to_ao_range(shell_mask[0]).second;
 
-  return submat_map;
+  const int block_size = 512;
+  std::vector< std::pair<int32_t, int32_t> > submat_map_expand;
+  int small_index = 0;
+  bool passedBlock = false;
+  int delta;
+  for (int i = 0; i < submat_map.size(); i++) {
+    int start = submat_map[i].first;
+    int end   = submat_map[i].second;
+    while ((start / block_size) < (end / block_size)) {
+      int next_end = ((start / block_size) + 1) * block_size;
 
+      submat_map_expand.emplace_back(start, next_end);
+
+      delta = next_end - start;
+      submat_map_expand.emplace_back(small_index, small_index + delta);
+      small_index += delta;
+
+      if (start >= block_size && !passedBlock) {
+        submat_map_expand[1].second = i;
+	passedBlock = true;
+      }
+
+      start = next_end;
+    }
+
+    if (start != end) {
+       submat_map_expand.emplace_back(start, end);
+
+      delta = end - start;
+      submat_map_expand.emplace_back(small_index, small_index + delta);
+      small_index += delta;
+
+
+      if (start >= block_size && !passedBlock) {
+        submat_map_expand[1].second = i;
+	passedBlock = true;
+      }
+    }
+
+  }
+
+
+  return submat_map_expand;
 }
 
 
