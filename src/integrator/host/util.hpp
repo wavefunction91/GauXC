82125c08ef2fd7395b1e19c05e287e4955757980
David Clark <daclark@nvidia.com>
2020-11-05 12:24:07 -0800

    Adjusts host code to use arrays instead of pairs

--- src/integrator/host/util.hpp
+++ src/integrator/host/util.hpp
@@ -11,7 +11,7 @@ template <typename _F1, typename _F2>
 void submat_set(int32_t M, int32_t N, int32_t MSub, 
   int32_t NSub, _F1 *ABig, int32_t LDAB, _F2 *ASmall, 
   int32_t LDAS, 
-  std::vector<std::pair<int32_t,int32_t>> &submat_map) {
+  std::vector<std::array<int32_t,3>> &submat_map) {
 
   (void)(M);
   (void)(N);
@@ -20,13 +20,13 @@ void submat_set(int32_t M, int32_t N, int32_t MSub,
 
   int32_t i(0);
   for( auto& iCut : submat_map ) {
-    int32_t deltaI = iCut.second - iCut.first;
+    int32_t deltaI = iCut[1];
     int32_t j(0);
   for( auto& jCut : submat_map ) {
-    int32_t deltaJ = jCut.second - jCut.first;
+    int32_t deltaJ = jCut[1];
   
-    auto* ABig_use   = ABig   + iCut.first + jCut.first * LDAB;
-    auto* ASmall_use = ASmall + i          + j          * LDAS;
+    auto* ABig_use   = ABig   + iCut[0] + jCut[0] * LDAB;
+    auto* ASmall_use = ASmall + i       + j       * LDAS;
 
 
     GauXC::blas::lacpy( 'A', deltaI, deltaJ, ABig_use, LDAB, 
@@ -45,7 +45,7 @@ template <typename _F1, typename _F2>
 void inc_by_submat(int32_t M, int32_t N, int32_t MSub, 
   int32_t NSub, _F1 *ABig, int32_t LDAB, _F2 *ASmall, 
   int32_t LDAS, 
-  std::vector<std::pair<int32_t,int32_t>> &submat_map) {
+  std::vector<std::array<int32_t,3>> &submat_map) {
 
   (void)(M);
   (void)(N);
@@ -54,13 +54,13 @@ void inc_by_submat(int32_t M, int32_t N, int32_t MSub,
 
   int32_t i(0);
   for( auto& iCut : submat_map ) {
-    int32_t deltaI = iCut.second - iCut.first;
+    int32_t deltaI = iCut[1];
     int32_t j(0);
   for( auto& jCut : submat_map ) {
-    int32_t deltaJ = jCut.second - jCut.first;
+    int32_t deltaJ = jCut[1];
   
-    auto* ABig_use   = ABig   + iCut.first + jCut.first * LDAB;
-    auto* ASmall_use = ASmall + i          + j          * LDAS;
+    auto* ABig_use   = ABig   + iCut[0] + jCut[0] * LDAB;
+    auto* ASmall_use = ASmall + i       + j       * LDAS;
 
 
     for( int32_t jj = 0; jj < deltaJ; ++jj )
