547f189d0a6165469e070d7b81355748dc51852d
David Clark <daclark@nvidia.com>
2020-11-16 10:54:14 -0800

    Adds weight block size tuning

--- src/integrator/cuda/cuda_device_properties.hpp
+++ src/integrator/cuda/cuda_device_properties.hpp
@@ -10,8 +10,12 @@ static constexpr uint32_t max_warps_per_thread_block =
   max_threads_per_thread_block / warp_size;
 
 static constexpr uint32_t max_submat_blocks = 10;
+
+// Properties for weight algorithm
 static constexpr uint32_t weight_unroll = 2;
 static_assert(weight_unroll == 2, "Weight unroll is only tested for value of 2");
+static constexpr uint32_t weight_thread_block = 640;
+static constexpr uint32_t weight_thread_block_per_sm = 2;
 
 uint32_t get_submat_cut_block(int32_t LDA);
 uint32_t get_device_sm_count();
