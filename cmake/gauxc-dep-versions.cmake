fdb92c1cc264bef9efbfc522a5dcaaf0485be59b
David Williams-Young <dbwy@lbl.gov>
2021-01-13 17:03:06 -0500

    [CMAKE] Bump ExchCXX + IntegratorXX versions

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -8,10 +8,10 @@ set( GAUXC_CUB_REPOSITORY https://github.com/NVIDIA/cub.git )
 set( GAUXC_CUB_REVISION   1.10.0 )
 
 set( GAUXC_EXCHCXX_REPOSITORY https://github.com/wavefunction91/ExchCXX.git )
-set( GAUXC_EXCHCXX_REVISION   cmake-fixup )
+set( GAUXC_EXCHCXX_REVISION   8966c818fe7531f3e85c0d8bc600bc310a99ba65 )
 
 set( GAUXC_GAU2GRID_REPOSITORY https://github.com/dgasmith/gau2grid.git )
 set( GAUXC_GAU2GRID_REVISION   v2.0.6 )
 
 set( GAUXC_INTEGRATORXX_REPOSITORY https://github.com/wavefunction91/IntegratorXX.git )
-set( GAUXC_INTEGRATORXX_REVISION   cmake-fixup )
+set( GAUXC_INTEGRATORXX_REVISION   c1fbe48361793044981ea6134fea5837f5338316 )
