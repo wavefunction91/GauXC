e38ab2d59b0d9993f50341fe4fba391f1ebf8279
David Williams-Young <dbwy@lbl.gov>
2021-01-14 19:14:27 +0000

    [CI] Fix CI with Fortran compiler

--- .github/workflows/scripts/compiler_setup.sh
+++ .github/workflows/scripts/compiler_setup.sh
@@ -5,8 +5,9 @@ echo $CXX
 
 export TOOLCHAIN=$1
 
-sudo apt install ${CC} ${CXX}
+sudo apt install ${CC} ${CXX} ${FC}
 
 sed -i -e "s/CI_CXX/${CXX}/g"  ${TOOLCHAIN}
 sed -i -e "s/CI_C/${CC}/g" ${TOOLCHAIN}
+sed -i -e "s/CI_FC/${FC}/g" ${TOOLCHAIN}
 echo $PWD && cat ${TOOLCHAIN}
