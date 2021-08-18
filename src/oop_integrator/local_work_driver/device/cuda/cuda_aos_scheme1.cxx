55979d0fea00cee4a5a7a8ddfe84a9277fd58f2b
David Williams-Young <dbwy@uw.edu>
2021-08-18 16:57:00 -0700

    Collocation kernels in OOP refactor

--- src/oop_integrator/local_work_driver/device/cuda/cuda_aos_scheme1.cxx
+++ src/oop_integrator/local_work_driver/device/cuda/cuda_aos_scheme1.cxx
@@ -1,10 +1,50 @@
 #include "cuda_aos_scheme1.hpp"
+#include "kernels/collocation_device.hpp"
 
 namespace GauXC {
 namespace detail {
 
 std::unique_ptr<XCDeviceData> CudaAoSScheme1::create_device_data() {
   return std::make_unique<Data>();
+}
+
+
+void CudaAoSScheme1::eval_collocation( XCDeviceData* _data ) {
+
+  auto* data = dynamic_cast<Data*>(_data);
+  if( !data ) throw std::runtime_error("BAD DATA CAST");
+
+  auto tasks = data->host_device_tasks;
+  const auto ntasks = tasks.size();
+
+  size_t npts_max, nshells_max;
+  for( auto& task : tasks ) {
+    npts_max    = std::max( npts_max, task.npts );
+    nshells_max = std::max( nshells_max, task.nshells );
+  }
+
+  eval_collocation_masked_combined( ntasks, npts_max, nshells_max,
+    data->shells_device, data->device_tasks, *data->master_stream );
+
+}
+
+void CudaAoSScheme1::eval_collocation_gradient( XCDeviceData* _data ) {
+
+  auto* data = dynamic_cast<Data*>(_data);
+  if( !data ) throw std::runtime_error("BAD DATA CAST");
+
+  auto tasks = data->host_device_tasks;
+  const auto ntasks = tasks.size();
+
+  size_t npts_max, nshells_max;
+  for( auto& task : tasks ) {
+    npts_max    = std::max( npts_max, task.npts );
+    nshells_max = std::max( nshells_max, task.nshells );
+  }
+
+  eval_collocation_masked_combined_deriv1( ntasks, npts_max, nshells_max,
+    data->shells_device, data->device_tasks, *data->master_stream );
+
 }
 
 }
