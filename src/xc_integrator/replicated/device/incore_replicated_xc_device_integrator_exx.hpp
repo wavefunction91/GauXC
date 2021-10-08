799384e5ed1276f47e18a1f4392b4efcd905180c
David Williams-Young <dbwy@uw.edu>
2021-10-08 10:51:49 -0700

    Refactor repliated device source for easier EXC grad development

---
+++ src/xc_integrator/replicated/device/incore_replicated_xc_device_integrator_exx.hpp
@@ -0,0 +1,21 @@
+
+#include "incore_replicated_xc_device_integrator.hpp"
+#include "device/local_device_work_driver.hpp"
+#include <stdexcept>
+#include "device/xc_device_aos_data.hpp"
+#include <fstream>
+
+namespace GauXC  {
+namespace detail {
+
+template <typename ValueType>
+void IncoreReplicatedXCDeviceIntegrator<ValueType>::
+  eval_exx_( int64_t m, int64_t n, const value_type* P,
+             int64_t ldp, value_type* K, int64_t ldk, 
+             const IntegratorSettingsEXX& settings ) { 
+             
+  throw std::runtime_error(std::string(__PRETTY_FUNCTION__) + " NYI" );                 
+}
+
+}
+}
