64f5610dfc6f96bab47aafa0dbadf829993c3c8e
thom-popovici <dorupopovici13@gmail.com>
2021-12-14 19:03:43 -0800

    one header file chebyshev_boys_computation has been updated in the .cxx file

--- src/xc_integrator/local_work_driver/host/reference_local_host_work_driver.cxx
+++ src/xc_integrator/local_work_driver/host/reference_local_host_work_driver.cxx
@@ -9,7 +9,7 @@
 #include <gauxc/basisset_map.hpp>
 #include "rys_integral.h"
 #include "obara_saika_integrals.h"
-#include "chebyshev_boys_function.hpp"
+#include "chebyshev_boys_computation.hpp"
 #include <gauxc/util/real_solid_harmonics.hpp>
 #include "integrator_util/integral_bounds.hpp"
 
