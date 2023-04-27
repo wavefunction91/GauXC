42f623acb4b9321e7f0bed7d013ac0eec645cb21
Ajay Panyala <ajay.panyala@gmail.com>
2023-04-27 10:01:15 -0700

    use exchcxx fork of dmejiar for now

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -1,5 +1,5 @@
-set( GAUXC_LINALG_MODULES_REPOSITORY https://github.com/ajaypanyala/linalg-cmake-modules )
-set( GAUXC_LINALG_MODULES_REVISION  main ) 
+set( GAUXC_LINALG_MODULES_REPOSITORY https://github.com/wavefunction91/linalg-cmake-modules.git )
+set( GAUXC_LINALG_MODULES_REVISION  ab47b9e329b524cab3855861c7c70f26cd8c3704 ) 
 
 set( GAUXC_CEREAL_REPOSITORY https://github.com/USCiLab/cereal.git )
 set( GAUXC_CEREAL_REVISION   v1.3.0 )
@@ -7,8 +7,8 @@ set( GAUXC_CEREAL_REVISION   v1.3.0 )
 set( GAUXC_CUB_REPOSITORY https://github.com/NVIDIA/cub.git )
 set( GAUXC_CUB_REVISION   1.10.0 )
 
-set( GAUXC_EXCHCXX_REPOSITORY https://github.com/wavefunction91/ExchCXX.git )
-set( GAUXC_EXCHCXX_REVISION   8966c818fe7531f3e85c0d8bc600bc310a99ba65 )
+set( GAUXC_EXCHCXX_REPOSITORY https://github.com/dmejiar/ExchCXX.git )
+set( GAUXC_EXCHCXX_REVISION   master )
 
 set( GAUXC_GAU2GRID_REPOSITORY https://github.com/dgasmith/gau2grid.git )
 set( GAUXC_GAU2GRID_REVISION   v2.0.6 )
