2e1ff047bc130952762b5abc3f2e69576740f500
David Williams-Young <dbwy@lbl.gov>
2023-08-19 16:28:56 -0700

    Make HDF5 Enablement Less Strict (#71)
    * Make HDF5 enablment less strict, only build bindings if enabled + found
    
    * Fixup: ensure config file is generated after GAUXC_HAS_HDF5 is set

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -18,3 +18,6 @@ set( GAUXC_GAU2GRID_REVISION   v2.0.6 )
 
 set( GAUXC_INTEGRATORXX_REPOSITORY https://github.com/wavefunction91/IntegratorXX.git )
 set( GAUXC_INTEGRATORXX_REVISION   ea07dedd37e7bd49ea06394eb811599002b34b49 )
+
+set( GAUXC_HIGHFIVE_REPOSITORY https://github.com/BlueBrain/HighFive.git )
+set( GAUXC_HIGHFIVE_REVISION 805f0e13d09b47c4b01d40682621904aa3b31bb8 )
