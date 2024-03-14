2eb816cddc6123a1be4d0b29e9ab930cb31fd42d
David Williams-Young <dbwy@lbl.gov>
2024-03-14 09:54:30 -0700

    [BREAKS DEVICE CODE] Remove nprim_pair_max in favor of dynamic allocations in ShellPair, CPU sn-K tests pass

--- include/gauxc/shell_pair.hpp
+++ include/gauxc/shell_pair.hpp
@@ -16,8 +16,6 @@ namespace detail {
     double x, y, z;
   };
 
-  static constexpr size_t nprim_pair_max = 64ul;
-
   template <typename Integral>
   inline intmax_t csr_index( size_t i, size_t j, Integral* row_ptr, Integral* col_ind ) {
     const auto j_st = col_ind + row_ptr[i];
@@ -47,8 +45,7 @@ class ShellPair {
   using shell_type = Shell<F>;
   using const_shell_ref = const shell_type&;
 
-  std::array< PrimitivePair<F>, detail::nprim_pair_max > prim_pairs_;
-  size_t nprim_pairs_ = 0;
+  std::vector<PrimitivePair<F>> prim_pairs_;
 
   void generate( const_shell_ref bra, const_shell_ref ket ) {
 
@@ -63,13 +60,10 @@ class ShellPair {
 
     const auto np_bra = bra.nprim();
     const auto np_ket = ket.nprim();
-    nprim_pairs_ = 0;
     for( auto i = 0; i < np_bra; ++i )
     for( auto j = 0; j < np_ket; ++j ) {
-      if( nprim_pairs_ >= detail::nprim_pair_max ) 
-        GAUXC_GENERIC_EXCEPTION("Too Many Primitive Pairs");
 
-      auto& pair = prim_pairs_[nprim_pairs_];
+      auto& pair = prim_pairs_.emplace_back();
       const auto alpha_bra = bra.alpha()[i];
       const auto alpha_ket = ket.alpha()[j];
 
@@ -80,9 +74,9 @@ class ShellPair {
         bra.coeff()[i] * ket.coeff()[j] *
         std::exp( -alpha_bra * alpha_ket * dAB * oo_g );
 
+      // TODO Make configurable
       if(std::abs(Kab) < 1e-12) continue;
 
-      nprim_pairs_++;
       pair.P.x = (alpha_bra * A.x + alpha_ket * B.x) * oo_g;
       pair.P.y = (alpha_bra * A.y + alpha_ket * B.y) * oo_g;
       pair.P.z = (alpha_bra * A.z + alpha_ket * B.z) * oo_g;
@@ -103,17 +97,17 @@ class ShellPair {
 
 public:
 
-  ShellPair( ) : nprim_pairs_(0) {}
+  ShellPair() = default;
 
   ShellPair( const Shell<F>& bra, const Shell<F>& ket ) {
     if( bra.l() >= ket.l() ) generate(bra,ket);
     else                     generate(ket,bra);
   }
 
-  inline HOST_DEVICE_ACCESSIBLE PrimitivePair<F>* prim_pairs() { return detail::contiguous_data(prim_pairs_); }
-  inline HOST_DEVICE_ACCESSIBLE const PrimitivePair<F>* prim_pairs() const { return detail::contiguous_data(prim_pairs_); }
+  inline PrimitivePair<F>* prim_pairs() { return prim_pairs_.data(); }
+  inline const PrimitivePair<F>* prim_pairs() const { return prim_pairs_.data(); }
 
-  inline HOST_DEVICE_ACCESSIBLE size_t nprim_pairs() const { return nprim_pairs_; }
+  inline size_t nprim_pairs() const { return prim_pairs_.size(); }
 
 };
 
