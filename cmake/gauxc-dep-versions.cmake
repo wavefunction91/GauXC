9acd899b04bc44096ef9a33edf9c4624950f9610
Jorge Luis Galvez Vallejo <jg4@iastate.edu>
2025-03-23 20:08:18 -0700

    fix

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -2,7 +2,7 @@ set( GAUXC_LINALG_MODULES_REPOSITORY https://github.com/wavefunction91/linalg-cm
 set( GAUXC_LINALG_MODULES_REVISION  28ceaa9738f14935aa544289fa2fe4c4cc955d0e )
 
 set( GAUXC_CEREAL_REPOSITORY https://github.com/USCiLab/cereal.git )
-set( GAUXC_CEREAL_REVISION   v1.3.2 )
+set( GAUXC_CEREAL_REVISION   v1.3.0 )
 
 set( GAUXC_CUB_REPOSITORY https://github.com/NVIDIA/cub.git )
 set( GAUXC_CUB_REVISION   1.10.0 )
@@ -10,8 +10,8 @@ set( GAUXC_CUB_REVISION   1.10.0 )
 set( GAUXC_CUTLASS_REPOSITORY https://github.com/NVIDIA/cutlass.git )
 set( GAUXC_CUTLASS_REVISION v2.10.0 )
 
-set( GAUXC_EXCHCXX_REPOSITORY https://github.com/ryanstocks00/ExchCXX.git )
-set( GAUXC_EXCHCXX_REVISION 8a0004609afc710bdad4367867026a9daa0a758b)
+set( GAUXC_EXCHCXX_REPOSITORY https://github.com/JorgeG94/ExchCXX.git )
+set( GAUXC_EXCHCXX_REVISION 619a5778032f33fcf03df6d24be838b510e51bfb)
 
 set( GAUXC_GAU2GRID_REPOSITORY https://github.com/dgasmith/gau2grid.git )
 set( GAUXC_GAU2GRID_REVISION   v2.0.6 )
