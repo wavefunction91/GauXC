351e227dd8b22aaed856d2da9d3f62f648af95a5
David Williams-Young <davidwillia@microsoft.com>
2025-11-26 09:20:55 -0800

    Cereal begone!

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -1,9 +1,6 @@
 set( GAUXC_LINALG_MODULES_REPOSITORY https://github.com/wavefunction91/linalg-cmake-modules.git )
 set( GAUXC_LINALG_MODULES_REVISION  9d2c273a671d6811e9fd432f6a4fa3d915b144b8 )
 
-set( GAUXC_CEREAL_REPOSITORY https://github.com/USCiLab/cereal.git )
-set( GAUXC_CEREAL_REVISION   v1.3.0 )
-
 set( GAUXC_CUB_REPOSITORY https://github.com/NVIDIA/cub.git )
 set( GAUXC_CUB_REVISION   1.10.0 )
 
