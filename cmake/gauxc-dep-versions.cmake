8393072dca879ab818900a7c5ca2fce6b3a4d4f8
Loris Ercole <v-lercole@microsoft.com>
2026-06-10 20:05:09 +0200

    Update dependency hashes

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -1,5 +1,5 @@
 set( GAUXC_LINALG_MODULES_REPOSITORY https://github.com/wavefunction91/linalg-cmake-modules.git )
-set( GAUXC_LINALG_MODULES_REVISION  9d2c273a671d6811e9fd432f6a4fa3d915b144b8 )
+set( GAUXC_LINALG_MODULES_REVISION  222364df5e7639f371bf2f37ceb0f476301101a1 )
 
 set( GAUXC_CUB_REPOSITORY https://github.com/NVIDIA/cub.git )
 set( GAUXC_CUB_REVISION   1.10.0 )
@@ -8,13 +8,13 @@ set( GAUXC_CUTLASS_REPOSITORY https://github.com/NVIDIA/cutlass.git )
 set( GAUXC_CUTLASS_REVISION v2.10.0 )
 
 set( GAUXC_EXCHCXX_REPOSITORY https://github.com/lorisercole/ExchCXX.git )
-set( GAUXC_EXCHCXX_REVISION   20a2d6052bc5b1bac4a0d028f3735056e5345dac )
+set( GAUXC_EXCHCXX_REVISION   fab59dc41ef881e0a2b776a92b47c03101340071 )
 
 set( GAUXC_GAU2GRID_REPOSITORY https://github.com/dgasmith/gau2grid.git )
 set( GAUXC_GAU2GRID_REVISION   v2.0.6 )
 
 set( GAUXC_INTEGRATORXX_REPOSITORY https://github.com/lorisercole/IntegratorXX.git )
-set( GAUXC_INTEGRATORXX_REVISION   81e283d20eb3ce3bca49e79926a92801c642c2c5 )
+set( GAUXC_INTEGRATORXX_REVISION   923125236ea5971ee9accdea39da552b8e322ff6 )
 
 set( GAUXC_HIGHFIVE_REPOSITORY https://github.com/highfive-devs/HighFive.git )
 set( GAUXC_HIGHFIVE_REVISION 805f0e13d09b47c4b01d40682621904aa3b31bb8 )
